ec812fd1a74761f2cb573f571c612bbfd59836ac
AIsagulyan <aisagulyan09@gmail.com>
2026-09-16 18:15:26 +0000

    a

--- Java/101_Lecture_Notes/LectureInput.java
+++ Java/101_Lecture_Notes/LectureInput.java
@@ -1,9 +1,33 @@
 /*
     Lecture note example - Input!!
 */
-
+import java.util.Scanner
 class LectureInput{
     public static void main(String args[]) {
         // Your Code Goes here!
-	}
-}
+	}System.out.printl
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    System.out.println(item3Price);
+
+
+    System.out.println(" How many cheeseburger woould you like");
+    Scanner sc = new Scanner(System.in);
+
+
+    int item1q
